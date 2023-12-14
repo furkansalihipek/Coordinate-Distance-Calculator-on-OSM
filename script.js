8ca2730ed5ae49514e4dd6ece14ad7b1159d1cb7
Furkan <67519099+furkansalihipek@users.noreply.github.com>
2023-12-15 00:29:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 function calculateDistance() {
-    const lat1 = parseFloat(document.getElementById("lat1").value);
     // Kullanıcıdan birinci noktanın enlemini alıp float'a çevirip lat1 değişkenine atıyor
-    const lon1 = parseFloat(document.getElementById("lon1").value);
+    const lat1 = parseFloat(document.getElementById("lat1").value);
     // Kullanıcıdan birinci noktanın boylamını alıp float'a çevirip lon1 değişkenine atıyor
-    const lat2 = parseFloat(document.getElementById("lat2").value);
+    const lon1 = parseFloat(document.getElementById("lon1").value);
     // Kullanıcıdan ikinci noktanın enlemini alıp float'a çevirip lat2 değişkenine atıyor
-    const lon2 = parseFloat(document.getElementById("lon2").value);
+    const lat2 = parseFloat(document.getElementById("lat2").value);
     // Kullanıcıdan ikinci noktanın boylamını  alıp float'a çevirip lon2 değişkenine atıyor
+    const lon2 = parseFloat(document.getElementById("lon2").value);
 
+    // lat1, lon1, lat2, lon2 değerlerinden herhangi biri sayı değil ise mesaj verip boş haritaya döndürüyor
     if (isNaN(lat1) || isNaN(lon1) || isNaN(lat2) || isNaN(lon2)) {
         alert("Lütfen geçerli sayısal değerler girin.");
         return;
     }
-    // lat1, lon1, lat2, lon2 değerlerinden herhangi biri sayı değil ise mesaj verip boş haritaya döndürüyor
 
-    const coord1 = { lat: lat1, lon: lon1 };
     //lat1 ve lon1 değerlerini coord1 değişkenine atıyor
-    const coord2 = { lat: lat2, lon: lon2 };
+    const coord1 = { lat: lat1, lon: lon1 };
     //lat2 ve lon2 değerlerini coord2 değişkenine atıyor
-
+    const coord2 = { lat: lat2, lon: lon2 };
+    
+    //Bulduğu sonucu result değişkenine atıyor
     const result = haversineDistance(coord1, coord2);
+    
+    const mesafe = document.getElementById("empty-span");
+    console.log(mesafe.innerHTML);
+    mesafe.innerText = `${result.toFixed(2)} km`
 
+    // Bulduğu uzaklık sonucunu km cinsinden konsola yazıyor
     console.log(`İki nokta arasındaki doğrudan uzaklık: ${result.toFixed(2)} km`);
-    // Bulduğu uzaklık sonucunu km cinsinden ekrana yazıyor
-    alert(`İki nokta arasındaki doğrudan uzaklık: ${result.toFixed(2)} km`);
 
     const map = L.map('map').setView([lat1, lon1], 13);
 
@@ -60,3 +64,6 @@ function haversineDistance(coord1, coord2) {
 function deg2rad(deg) {
     return deg * (Math.PI / 180);
 }
+
+
+
